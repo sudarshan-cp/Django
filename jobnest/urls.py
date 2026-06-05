c963250e8696e003f94ccb52c3038c0f5879047d
Sudarshan <sudarshancpoojary@gmail.com>
2026-06-05 22:25:21 +0530

    Rebrand Django admin panel to Jobnest

--- jobnest/urls.py
+++ jobnest/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Jobnest Administration"
+admin.site.site_title = "Jobnest Admin"
+admin.site.index_title = "Welcome to Jobnest"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.Acc_urls")),
